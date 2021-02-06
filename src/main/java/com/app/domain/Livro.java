fe00f5d5a8e262eba3e0e3f646c98aaa26377797
denilsouza <denilson.souza@gmai.com>
2021-02-06 17:24:07 -0300

    finalizando classes de  dominio

--- src/main/java/com/app/domain/Livro.java
+++ src/main/java/com/app/domain/Livro.java
@@ -1,12 +1,24 @@
 package com.app.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Livro {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String titulo;
 	private String nomeAutor;
 	private String texto;
 
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
 
 	public Livro() {
